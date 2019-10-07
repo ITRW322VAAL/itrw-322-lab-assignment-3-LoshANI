8960b406ffbdaebc16a9b353e285e560dce8cf74
28230175 <loshanikwinda@gmail.com>
2019-10-07 18:25:16 +0200

    28230175 Client.py updated

---
+++ LAB3/28230175_Sockets/Client.py
@@ -0,0 +1,13 @@
+
+open
+import socket
+
+HOST = '127.0.0.1' #The server host or IP address
+PORT = 9999 #The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOXL_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, Server')
+    data = s.recv(1024)
+
+print ('Received', repr(data))
